9b242123c3362386b2657444faa02ced9f2cbebf
Sahil Das <das611821@gmail.com>
2024-07-21 13:25:15 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Form from "./components/Form"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center  p-8">
+      <Form />
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
